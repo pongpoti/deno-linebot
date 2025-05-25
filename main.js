00bb65af9e6ce774a3eccf5354e83b2733d79f17
pongpoti <pong.poti@gmail.com>
2025-05-25 22:23:23 +0700

    c

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-import * as line from '@line/bot-sdk'
-import express from 'express'
+import * as line from '@line/bot-sdk';
+import express from 'express';
+import "jsr:@std/dotenv/load";
 
-// create LINE SDK config from env variables
+Deno.env.set("CHANNEL_SECRET", "c02f0c8c17e4b0f607b31e3ad1c3f529");
+Deno.env.set("CHANNEL_ACCESS_TOKEN", "qmdRNYKVnChOLXdfdhFn159TMJtURVZ1wpx2cp9EKLCTv2NWq14J+OFjtOWObAKVPmY8+q16zF14O55JXI83c9lBEtFgV31unhTx4lDpQPptfzK+G8ANFSkHA08qx82xnL8gmEyPKiRoZVhjVrBcOQdB04t89/1O/w1cDnyilFU=");
+
+console.log(Deno.env.get("CHANNEL_ACCESS_TOKEN"));
+
+
+
 const config = {
-  channelSecret: process.env.CHANNEL_SECRET || 'c02f0c8c17e4b0f607b31e3ad1c3f529',
+  channelSecret: Deno.env.get("CHANNEL_SECRET")
 };
 
-// create LINE SDK client
 const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || 'qmdRNYKVnChOLXdfdhFn159TMJtURVZ1wpx2cp9EKLCTv2NWq14J+OFjtOWObAKVPmY8+q16zF14O55JXI83c9lBEtFgV31unhTx4lDpQPptfzK+G8ANFSkHA08qx82xnL8gmEyPKiRoZVhjVrBcOQdB04t89/1O/w1cDnyilFU='
 });
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -27,24 +29,19 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  // create an echoing text message
   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
   return client.replyMessage({
     replyToken: event.replyToken,
     messages: [echo],
   });
 }
 
-// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);

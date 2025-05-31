5ce411519f096a095c90714619af59ba83cea80d
pongpoti <pong.poti@gmail.com>
2025-06-01 00:30:08 +0700

    c

--- main.js
+++ main.js
@@ -94,12 +94,3 @@ function handleEvent(event) {
     messages: [echo],
   });
 }
-
-function randomStr(len, arr) {
-    let ans = '';
-    for (let i = len; i > 0; i--) {
-        ans +=
-            arr[(Math.floor(Math.random() * arr.length))];
-    }
-    console.log(ans);
-}
